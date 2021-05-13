5ab3f3ee48f11c447d05533bbc3309e84b1817b0
iamceege <hello@calebjacob.com>
2021-05-13 00:47:17 -0600

    Improving FE linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,11 @@ module.exports = {
   },
 
   extends: [
-    // 'plugin:vue/vue3-essential',
-    'plugin:vue/vue3-recommended',
-    '@vue/prettier',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier/@typescript-eslint'
+    "plugin:vue/vue3-recommended",
+    "@vue/prettier",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier/@typescript-eslint"
   ],
 
   parserOptions: {
@@ -24,55 +23,55 @@ module.exports = {
   },
 
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: "@typescript-eslint/parser"
   },
 
   rules: {
-    'no-console': 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-useless-catch': 'off',
-    'prettier/prettier': [
-      'error',
+    "no-console": "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-useless-catch": "off",
+    "prettier/prettier": [
+      "error",
       {
         singleQuote: true,
-        trailingComma: 'none',
+        trailingComma: "none",
         vueIndentScriptAndStyle: true
       }
     ],
-    'vue/attribute-hyphenation': ['error'],
-    'vue/component-name-in-template-casing': [
-      'error',
-      'kebab-case',
+    "vue/attribute-hyphenation": ["error"],
+    "vue/component-name-in-template-casing": [
+      "error",
+      "kebab-case",
       {
         registeredComponentsOnly: false,
         ignores: []
       }
     ],
-    'vue/prop-name-casing': ['error', 'camelCase'],
-    'vue/script-indent': 'off',
-    'vue/html-self-closing': ['error', {
-      'html': {
-        'void': 'always',
-        'normal': 'always',
-        'component': 'always'
+    "vue/prop-name-casing": ["error", "camelCase"],
+    "vue/script-indent": "off",
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "always",
+        "component": "always"
       },
-      'svg': 'always',
-      'math': 'always'
+      "svg": "always",
+      "math": "always"
     }],
-    '@typescript-eslint/explicit-module-boundary-types': [
-      'error'
+    "@typescript-eslint/explicit-module-boundary-types": [
+      "error"
     ],
-    '@typescript-eslint/no-explicit-any': 'off'
+    "@typescript-eslint/no-explicit-any": "off"
   },
 
   overrides: [
     {
-      files: ['**/*.test.{j,t}s?(x)'],
+      files: ["**/*.test.{j,t}s?(x)"],
       env: {
         jest: true
       }
     }
   ],
 
-  ignorePatterns: ['dist/**/*'],
+  ignorePatterns: ["dist/**/*"]
 };
